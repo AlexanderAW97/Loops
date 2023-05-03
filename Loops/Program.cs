163506c219f20888d358370435c22ae43d333c7c
AlexanderAW97 <alexanderw@getacademy.no>
2023-05-03 14:20:00 +0200

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 5; i++) {
+                string Navn = "Alex!";
+                Console.WriteLine("Terje er kul");
+                Console.Write("");
+                Console.WriteLine(Navn[i]);
+            }
+
+
+            Console.WriteLine("");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
